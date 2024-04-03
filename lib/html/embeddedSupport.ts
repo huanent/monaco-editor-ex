2ecd5cf41d4c6293d7a431701a17d284cd596258
huanent <huanent@icloud.com>
2024-04-03 15:03:15 +0800

    Fixed code

--- lib/html/embeddedSupport.ts
+++ lib/html/embeddedSupport.ts
@@ -7,7 +7,6 @@ import { LanguageService, TokenType, Range, Position as HtmlPosition, TextDocume
 import { Position } from '../monaco';
 import { toLsPosition } from './utils';
 import { getDirectiveRegion } from './directive';
-import { ListFormat } from 'typescript';
 
 export interface LanguageRange extends Range {
 	languageId: string | undefined;
